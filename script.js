beb8bce6a9b7cfcb8b61bf5799cb5a8b19d2dbf5
mozahidul01 <mozo.postbox@gmail.com>
2020-08-03 15:21:40 +0600

    random number generator

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
+// Notification Hide
 document.getElementById("pin-unmatched").style.display = "none";
 document.getElementById("pin-matched").style.display = "none";
+
+// Random Number Generator
+function randomNumber() { 
+    var min = 100000; 
+    var max = 999999; 
+    var generateNumber =  Math.round(Math.random() * (max - min + 1)) + min; 
+
+    document.getElementById("random-number").value  = generateNumber;
+} 
+
